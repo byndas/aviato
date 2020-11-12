c5f0ebcca3bd27768eac6ed2c021a3a7e3c14bdf
lemokh <byndasma@gmail.com>
2020-11-12 18:09:30 +0400

    finishing touches, added name and title language inputs

--- src/footer/Footer.component.jsx
+++ src/footer/Footer.component.jsx
@@ -1,91 +1,120 @@
 import React, { useContext } from "react";
 import "./footer.styles.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faFacebook, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import {
+  faFacebook,
+  faInstagram,
+  faYoutube
+} from "@fortawesome/free-brands-svg-icons";
 import logo from "../images/logo.png";
 import { Link } from "react-router-dom";
 import translate from "../language/translate";
 import { LanguageContext } from "../context/LanguageContext";
 
-function Footer (){
-    const { language } = useContext(LanguageContext);
-    const { News, Home, AboutUs, Projects, Gallery, Catalog, Contact, Abouttext} = translate[language];
-    
-    return (
-      <div className="footer-dark">
-        <footer>
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-6 col-md-3 item mt-5 text-uppercase">
-                <ul onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}>
-                  <li>
-                    <Link className="nav-link" to="/">
-                     {Home} <span className="sr-only">(current)</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="nav-link" to="/news">
-                       {News} <span className="sr-only">(current)</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="nav-link" to="/about">
-                      {AboutUs} <span className="sr-only">(current)</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="nav-link" to="/projects">
-                      {Projects} <span className="sr-only">(current)</span>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-sm-6 col-md-3 item mt-5 text-uppercase">
-                <ul onClick={() => window.scrollTo({top: 0, behavior: 'smooth'})}>
-                  <li>
-                    <Link className="nav-link" to="/gallery">
-                      {Gallery} <span className="sr-only">(current)</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="nav-link" to="/catalog">
-                      {Catalog} <span className="sr-only">(current)</span>
-                    </Link>
-                  </li>
-                  <li>
-                    <Link className="nav-link" to="/contact">
-                      {Contact} <span className="sr-only">(current)</span>
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-md-6 item-text mt-n4">
-                <p className="heading-secondary">
-                  1*209{" "}
-                  <img alt="logo" style={{ width: "25px", height: "25px", marginBottom: "6px", borderRadius: '50%' }}
-                    src={logo} />
-                </p>
-                <p className="about text-center">{Abouttext}</p>
-              </div>
-              <div className="col item d-flex justify-content-center">
-                <a className="nav-link" href="https://www.youtube.com/">
-                  <FontAwesomeIcon className='footer_socials' icon={faYoutube} />
-                </a>
-                <a href="https://www.instagram.com/?hl=en" className="nav-link">
-                  <FontAwesomeIcon className='footer_socials' icon={faInstagram} />
-                </a>
-                <a href="https://www.facebook.com/LIVE.Branding.Official/posts/3465174490183797"
-                  className="nav-link">
-                  <FontAwesomeIcon className='footer_socials' icon={faFacebook} />
-                </a>
-              </div>
+function Footer() {
+  const { language } = useContext(LanguageContext);
+  const {
+    News,
+    Home,
+    AboutUs,
+    Projects,
+    Gallery,
+    Catalog,
+    Contact,
+    Abouttext
+  } = translate[language];
+
+  return (
+    <div className="footer-dark">
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-6 col-md-3 item mt-5 text-uppercase">
+              <ul
+                onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+              >
+                <li>
+                  <Link className="nav-link" to="/">
+                    {Home} <span className="sr-only">(current)</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" to="/news">
+                    {News} <span className="sr-only">(current)</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" to="/about">
+                    {AboutUs} <span className="sr-only">(current)</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" to="/projects">
+                    {Projects} <span className="sr-only">(current)</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-sm-6 col-md-3 item mt-5 text-uppercase">
+              <ul
+                onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+              >
+                <li>
+                  <Link className="nav-link" to="/gallery">
+                    {Gallery} <span className="sr-only">(current)</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" to="/catalog">
+                    {Catalog} <span className="sr-only">(current)</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link className="nav-link" to="/contact">
+                    {Contact} <span className="sr-only">(current)</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-6 item-text mt-n4">
+              <p className="heading-secondary">
+                1*209{" "}
+                <img
+                  alt="logo"
+                  style={{
+                    width: "25px",
+                    height: "25px",
+                    marginBottom: "6px",
+                    borderRadius: "50%"
+                  }}
+                  src={logo}
+                />
+              </p>
+              <p className="about text-center">{Abouttext}</p>
+            </div>
+            <div className="col item d-flex justify-content-center">
+              <a className="nav-link" href="https://www.youtube.com/">
+                <FontAwesomeIcon className="footer_socials" icon={faYoutube} />
+              </a>
+              <a href="https://www.instagram.com/?hl=en" className="nav-link">
+                <FontAwesomeIcon
+                  className="footer_socials"
+                  icon={faInstagram}
+                />
+              </a>
+              <a
+                href="https://www.facebook.com/LIVE.Branding.Official/posts/3465174490183797"
+                className="nav-link"
+              >
+                <FontAwesomeIcon className="footer_socials" icon={faFacebook} />
+              </a>
             </div>
           </div>
-        </footer>
-        <p className="copyright text-center">1*209 © 2020</p>
-      </div>
-    );
-  }
-
+        </div>
+      </footer>
+      <p className="copyright text-center">1*209 © 2020</p>
+    </div>
+  );
+}
 
 export default Footer;
--- src/pages/PagePosts.component.jsx
+++ src/pages/PagePosts.component.jsx
@@ -15,7 +15,19 @@ class PagePosts extends Component {
     this.handleDelete = this.handleDelete.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
-  handleEdit(id, src, textGeo, textEng, textRus, title, name) {
+  handleEdit(
+    id,
+    src,
+    textGeo,
+    textEng,
+    textRus,
+    nameGeo,
+    nameEng,
+    nameRus,
+    titleGeo,
+    titleEng,
+    titleRus
+  ) {
     // scrolls up to Form
     window.scrollTo(0, 0);
     // populates sibling Form.jsx state (via parent component)
@@ -26,17 +38,18 @@ class PagePosts extends Component {
       textGeo,
       textEng,
       textRus,
-      title,
-      name
+      nameGeo,
+      nameEng,
+      nameRus,
+      titleGeo,
+      titleEng,
+      titleRus
     };
-  
+
     this.props.editPostInputs(editObj);
   }
   handleDelete(id, src, pageName) {
-
-
     if (src !== null) {
-
       // DELETES IMAGE FROM FIREBASE STORAGE
       deleteImageFireStorage(src);
     }
@@ -52,8 +65,48 @@ class PagePosts extends Component {
     const { expanded } = this.state;
     const { language } = this.context;
     const { ReadMore } = translate[language];
-    const { auth, id, src, textGeo, textEng, textRus,title,name,pageName } = this.props;
-    const text = language === 'Geo' ? textGeo : language === 'Eng' ? textEng : language === 'Rus' ? textRus : ''
+    const {
+      auth,
+      id,
+      src,
+      textGeo,
+      textEng,
+      textRus,
+      titleGeo,
+      titleEng,
+      titleRus,
+      nameGeo,
+      nameEng,
+      nameRus,
+      pageName
+    } = this.props;
+
+    const text =
+      language === "Geo"
+        ? textGeo
+        : language === "Eng"
+        ? textEng
+        : language === "Rus"
+        ? textRus
+        : "";
+
+    const name =
+      language === "Geo"
+        ? nameGeo
+        : language === "Eng"
+        ? nameEng
+        : language === "Rus"
+        ? nameRus
+        : "";
+
+    const title =
+      language === "Geo"
+        ? titleGeo
+        : language === "Eng"
+        ? titleEng
+        : language === "Rus"
+        ? titleRus
+        : "";
 
     switch (pageName) {
       case "projects":
@@ -61,48 +114,64 @@ class PagePosts extends Component {
           <div>
             <div className="card project_content_title mt-5">
               <div className="card-header project_content_header text-uppercase">
-                <h5 className='project-name'>{name}</h5>
+                <h5 className="project-name">{name}</h5>
                 {auth && (
                   <div className="float-right">
                     <FontAwesomeIcon
                       type="button"
-                      onClick={() => {this.handleEdit( id, src, textGeo, textEng, textRus, title, name )}}
+                      onClick={() => {
+                        this.handleEdit(
+                          id,
+                          src,
+                          textGeo,
+                          textEng,
+                          textRus,
+                          nameGeo,
+                          nameEng,
+                          nameRus,
+                          titleGeo,
+                          titleEng,
+                          titleRus
+                        );
+                      }}
                       className="icons"
-                      icon={faEdit}/>
+                      icon={faEdit}
+                    />
                     <FontAwesomeIcon
                       type="button"
-                      onClick={() => {this.handleDelete(id, src, pageName)}}
+                      onClick={() => {
+                        this.handleDelete(id, src, pageName);
+                      }}
                       className="icons"
-                      icon={faTrash}/>
-                  </div>)}
+                      icon={faTrash}
+                    />
+                  </div>
+                )}
               </div>
               <h5 className="p-3 project-name">{title}</h5>
               <div className="card-body project-img-text">
-                  <img src={src} alt={name} className='rounded prj-image' />
+                <img src={src} alt={name} className="rounded prj-image" />
                 <div className="col-md-8">
-                  <p className="card-text">
-                  {text.substring(0, 400)}...
-                  </p>
+                  <p className="card-text">{text.substring(0, 400)}...</p>
                   <button
-                    style={{fontWeight: '500'}}
+                    style={{ fontWeight: "500" }}
                     onClick={this.handleClick}
                     className="btn shadow"
                     type="button"
                     data-toggle="collapse"
                     data-target={`#${id}`}
                     aria-expanded="false"
-                    aria-controls="collapseExample">
+                    aria-controls="collapseExample"
+                  >
                     {ReadMore}
                   </button>
                 </div>
               </div>
               <div className="collapse" id={id}>
-                  <div className="card-body">
-                    <p className="card-text">
-                  {text}
-                    </p>
-                  </div>
+                <div className="card-body">
+                  <p className="card-text">{text}</p>
                 </div>
+              </div>
             </div>
           </div>
         );
@@ -110,61 +179,92 @@ class PagePosts extends Component {
         return (
           <div className="container mb-3 shadow p-3 mb-5 rounded news_container">
             <h2 className="p-3 text-uppercase border-bottom">{name}</h2>
-              {auth && (
-                <div className="float-right">
-                  <FontAwesomeIcon
-                    type="button"
-                    onClick={() => {this.handleEdit( id, src, textGeo, textEng, textRus, title, name )}}
-                    icon={faEdit} />
-                  <FontAwesomeIcon
-                    type="button"
-                    onClick={() => { this.handleDelete(id, src, pageName) }}
-                    icon={faTrash} />
-                </div> )}
-              <p className="p-3 font-weight-bold">{title}</p>
-                  <div className='p_and_image card-body'>
-                   <img src={src} className="nws-image rounded" alt={name} />
-                     <p className="news-text">
-                     {text.substring(0, 400)}...
-                   </p>
-                </div>
-                <button 
-                  className={`${expanded ? 'btn read_more shadow rounded' : 'btn read_more rounded'}`}
-                  onClick={this.handleClick} 
+            {auth && (
+              <div className="float-right">
+                <FontAwesomeIcon
                   type="button"
-                  data-toggle="collapse"
-                  data-target={`#${id}`}
-                  aria-expanded="false"
-                  aria-controls="collapseExample">
-                  {ReadMore}
-                </button>
-                <div className="collapse" id={id}>
-                  <div className="card-body">
-                  {text}
-                  </div>
-                </div>
+                  onClick={() => {
+                    this.handleEdit(
+                      id,
+                      src,
+                      textGeo,
+                      textEng,
+                      textRus,
+                      nameGeo,
+                      nameEng,
+                      nameRus,
+                      titleGeo,
+                      titleEng,
+                      titleRus
+                    );
+                  }}
+                  icon={faEdit}
+                />
+                <FontAwesomeIcon
+                  type="button"
+                  onClick={() => {
+                    this.handleDelete(id, src, pageName);
+                  }}
+                  icon={faTrash}
+                />
+              </div>
+            )}
+            <p className="p-3 font-weight-bold">{title}</p>
+            <div className="p_and_image card-body">
+              <img src={src} className="nws-image rounded" alt={name} />
+              <p className="news-text">{text.substring(0, 400)}...</p>
             </div>
+            <button
+              className={`${
+                expanded
+                  ? "btn read_more shadow rounded"
+                  : "btn read_more rounded"
+              }`}
+              onClick={this.handleClick}
+              type="button"
+              data-toggle="collapse"
+              data-target={`#${id}`}
+              aria-expanded="false"
+              aria-controls="collapseExample"
+            >
+              {ReadMore}
+            </button>
+            <div className="collapse" id={id}>
+              <div className="card-body">{text}</div>
+            </div>
+          </div>
         );
       case "catalog":
         return (
           <div className="col mb-4">
             <div className="card shadow p-3 mb-5 rounded border-0 airplain_background">
               <div className="container text-center">
-                <img src={src} className="card-img-top rounded ctl-image" alt={name}/>
+                <img
+                  src={src}
+                  className="card-img-top rounded ctl-image"
+                  alt={name}
+                />
                 <div className="card-body">
                   <h5 className="card-title">{name}</h5>
-                  <p className={!expanded ? "fade text-truncate" : "card-text text-truncate"} >
-                     {text}
+                  <p
+                    className={
+                      !expanded
+                        ? "fade text-truncate"
+                        : "card-text text-truncate"
+                    }
+                  >
+                    {text}
                   </p>
                   <button
-                    style={{color: '#333', fontWeight: 500}}
+                    style={{ color: "#333", fontWeight: 500 }}
                     onClick={this.handleClick}
                     className="btn shadow"
                     type="button"
                     data-toggle="collapse"
                     data-target={`#${id}`}
                     aria-expanded="false"
-                    aria-controls="collapseExample" >
+                    aria-controls="collapseExample"
+                  >
                     {ReadMore}
                   </button>
                   {auth && (
@@ -178,12 +278,14 @@ class PagePosts extends Component {
                             textGeo,
                             textEng,
                             textRus,
-                            title,
-                            name
+                            nameGeo,
+                            nameEng,
+                            nameRus
                           );
                         }}
                         className="icons"
-                        icon={faEdit} />
+                        icon={faEdit}
+                      />
                       <FontAwesomeIcon
                         type="button"
                         onClick={() => {
@@ -197,9 +299,7 @@ class PagePosts extends Component {
                 </div>
                 <div className="collapse" id={id}>
                   <div className="card-body">
-                    <p className="card-text">
-                      {text}
-                    </p>
+                    <p className="card-text">{text}</p>
                   </div>
                 </div>
               </div>
@@ -208,38 +308,36 @@ class PagePosts extends Component {
         );
       case "gallery":
         return (
-  
-            <div className="carousel-item mt-5 mb-5 rounded">
-              <img src={src} className="gallery_img rounded" alt="..." />
-              <div id="flex" className="carousel-caption">
-                <p className="gallery_text font-italic text-center">
-                 {text}
-                  {auth && (
-                    <div>
-                      <FontAwesomeIcon
-                        style={{ color: "blue" }}
-                        type="button"
-                        onClick={() => {
-                          this.handleEdit(id, src, textGeo, textEng, textRus);
-                        }}
-                        className="icons"
-                        icon={faEdit}
-                      />
-                      <FontAwesomeIcon
-                        style={{ color: "blue" }}
-                        type="button"
-                        onClick={() => {
-                          this.handleDelete(id, src, pageName);
-                        }}
-                        className="icons"
-                        icon={faTrash}
-                      />
-                    </div>
-                  )}
-                </p>
-              </div>
+          <div className="carousel-item mt-5 mb-5 rounded">
+            <img src={src} className="gallery_img rounded" alt="..." />
+            <div id="flex" className="carousel-caption">
+              <p className="gallery_text font-italic text-center">
+                {text}
+                {auth && (
+                  <div>
+                    <FontAwesomeIcon
+                      style={{ color: "blue" }}
+                      type="button"
+                      onClick={() => {
+                        this.handleEdit(id, src, textGeo, textEng, textRus);
+                      }}
+                      className="icons"
+                      icon={faEdit}
+                    />
+                    <FontAwesomeIcon
+                      style={{ color: "blue" }}
+                      type="button"
+                      onClick={() => {
+                        this.handleDelete(id, src, pageName);
+                      }}
+                      className="icons"
+                      icon={faTrash}
+                    />
+                  </div>
+                )}
+              </p>
             </div>
-        
+          </div>
         );
       default:
         return;
@@ -248,4 +346,3 @@ class PagePosts extends Component {
 }
 
 export default PagePosts;
-
